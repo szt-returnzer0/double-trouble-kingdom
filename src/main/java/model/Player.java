34a91d141192367a017c1a9c823d1db0f20453ad
Neszlényi Kálmán Balázs <dpu51t@inf.elte.hu>
2022-03-10 17:26:11 +0100

    Implemented Player class

--- src/main/java/model/Player.java
+++ src/main/java/model/Player.java
@@ -1,4 +1,46 @@
 package model;
 
+import java.util.ArrayList;
+
 public class Player {
+    static private int number;
+    private final int playerNumber;
+    private final String name;
+    private final ArrayList<Entity> entities;
+    private int gold;
+
+    public Player(String name) {
+        this.playerNumber = ++number;
+        this.name = name;
+        this.gold = 100;
+        this.entities = new ArrayList<>(); // 1x Castle 2x Barrack
+    }
+
+    public int getPlayerNumber() {
+        return playerNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getGold() {
+        return gold;
+    }
+
+    public void addGold(int amount) {
+        this.gold += amount;
+    }
+
+    public ArrayList<Entity> getEntities() {
+        return entities;
+    }
+
+    public void addEntity(Entity entity) {
+        this.entities.add(entity);
+    }
+
+    public void removeEntity(Entity entity) {
+        this.entities.remove(entity);
+    }
 }
