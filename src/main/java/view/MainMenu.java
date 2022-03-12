8ebf2848e2ef95dc2e06034b68af2d5848dfe197
Neszlényi Kálmán Balázs <dpu51t@inf.elte.hu>
2022-03-12 19:47:00 +0100

    Added Main menu layout and MainMenu class

---
+++ src/main/java/view/MainMenu.java
@@ -0,0 +1,41 @@
+package view;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class MainMenu extends JPanel {
+    public MainMenu() {
+        this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+
+        JPanel topBar = new JPanel();
+        topBar.setLayout(new FlowLayout(FlowLayout.CENTER, 40, 10));
+        JLabel players = new JLabel("Játékos1 vs Játékos2", SwingConstants.CENTER);
+        topBar.add(players);
+        JLabel mapName = new JLabel("Pálya: Kezdő", SwingConstants.CENTER);
+        topBar.add(mapName);
+        this.add(topBar);
+
+        JPanel centerPanel = new JPanel();
+        centerPanel.setLayout(new BoxLayout(centerPanel, BoxLayout.Y_AXIS));
+        JLabel title = new JLabel("Double Trouble Kingdom");
+        title.setAlignmentX(Component.CENTER_ALIGNMENT);
+        centerPanel.add(title);
+
+        JPanel centerButtons = new JPanel();
+        centerButtons.setLayout(new FlowLayout());
+        JButton start = new JButton("Start");
+        centerButtons.add(start);
+
+        JButton editor = new JButton("Térképszerkesztő");
+        centerButtons.add(editor);
+        centerPanel.add(centerButtons);
+        this.add(centerPanel);
+
+        JPanel bottomButtons = new JPanel(new FlowLayout());
+        JButton rename = new JButton("Átnevezés");
+        bottomButtons.add(rename);
+        JButton mapSelect = new JButton("Pályaválasztás");
+        bottomButtons.add(mapSelect);
+        this.add(bottomButtons);
+    }
+}
--- src/main/java/view/MainWindow.java
+++ src/main/java/view/MainWindow.java
@@ -8,6 +8,9 @@ public class MainWindow {
         JFrame frame = new JFrame("Double Trouble Kingdom");
         frame.setPreferredSize(new Dimension(1920, 1080));
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setLayout(new BorderLayout());
+        MainMenu mainMenu = new MainMenu();
+        frame.add(mainMenu, BorderLayout.CENTER);
         frame.pack();
         frame.setVisible(true);
     }
