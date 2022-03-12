d2e247f772d7aa50febf72de5d09b0ff58c6fc08
Rubóczki Benedek <l705qg@inf.elte.hu>
2022-03-12 23:15:09 +0100

    Implemented startGame function for testing

--- src/main/java/view/MainWindow.java
+++ src/main/java/view/MainWindow.java
@@ -1,17 +1,52 @@
 package view;
 
+import model.*;
+import persistence.Database;
+
 import javax.swing.*;
 import java.awt.*;
 
 public class MainWindow {
+    private final JFrame frame;
+    private final MainMenu mainMenu;
+
     public MainWindow() {
-        JFrame frame = new JFrame("Double Trouble Kingdom");
-        frame.setPreferredSize(new Dimension(1920, 1080));
+        frame = new JFrame("Double Trouble Kingdom");
+        Dimension windowSize = new Dimension(1920, 1080);
+        frame.setPreferredSize(windowSize);
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         frame.setLayout(new BorderLayout());
-        MainMenu mainMenu = new MainMenu();
+        mainMenu = new MainMenu(this);
+
         frame.add(mainMenu, BorderLayout.CENTER);
         frame.pack();
         frame.setVisible(true);
+    }
+
+    public void startGame() {
+        frame.remove(mainMenu);
+
+        Database db = new Database();
+        Terrain[][] map = new Terrain[32][64];
+        for (int y = 0; y < 32; y++) {
+            for (int x = 0; x < 64; x++) {
+                switch ((int) (Math.random() * 4)) {
+                    case 0 -> map[y][x] = new Plains(new Point(x, y));
+                    case 1 -> map[y][x] = new Swamp(new Point(x, y));
+                    case 2 -> map[y][x] = new Desert(new Point(x, y));
+                    case 3 -> map[y][x] = new Mountain(new Point(x, y));
+                    default -> {
+                    }
+                }
+            }
+        }
+
+        String p1Name = "Játékos1";
+        String p2Name = "Játékos2";
+        Game game = new Game(db, map, p1Name, p2Name);
+        GameField gameField = new GameField(game, frame);
+
+        frame.add(gameField, BorderLayout.CENTER);
+        frame.pack();
     }
 }
