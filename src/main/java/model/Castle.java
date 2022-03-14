be32ed97122171bc0ecd9b637da382099fea149f
Rubóczki Benedek <l705qg@inf.elte.hu>
2022-03-14 17:23:49 +0100

    Fixed constructor (added type)

--- src/main/java/model/Castle.java
+++ src/main/java/model/Castle.java
@@ -6,8 +6,9 @@ public class Castle extends Building {
 
     public Castle(Point position, String side) {
         super(position);
+        this.type = "Castle";
         this.isAnimated = false;
         this.healthPoints = 100;
-        this.size = new Dimension(5, 5);
+        this.size = new Dimension(5, 15);
     }
 }
