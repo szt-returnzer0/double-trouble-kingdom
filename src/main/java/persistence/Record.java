4113edba2f63ed5daabdee09846bae6a4685ec69
Neszlényi Kálmán Balázs <dpu51t@inf.elte.hu>
2022-03-14 15:41:35 +0100

    Implemented Record class

--- src/main/java/persistence/Record.java
+++ src/main/java/persistence/Record.java
@@ -1,4 +1,25 @@
 package persistence;
 
-public class Record {
+public record Record(String p1Name, String p2Name, int winner, int time) {
+
+    public String getP1Name() {
+        return p1Name;
+    }
+
+    public String getP2Name() {
+        return p2Name;
+    }
+
+    public int getWinner() {
+        return winner;
+    }
+
+    public int getTime() {
+        return time;
+    }
+
+    @Override
+    public String toString() {
+        return "'" + p1Name + "','" + p2Name + "'," + winner + "," + time;
+    }
 }
