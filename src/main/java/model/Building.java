5b3097d43313c767b3a1f3a576f98843ded02c41
Neszlényi Kálmán Balázs <dpu51t@inf.elte.hu>
2022-03-16 17:51:38 +0100

    Implemented destroy and cleanup functions in Building class

--- src/main/java/model/Building.java
+++ src/main/java/model/Building.java
@@ -4,7 +4,24 @@ import java.awt.*;
 import java.io.Serializable;
 
 public abstract class Building extends Entity implements Serializable {
+    protected boolean canUpgrade; // false
+    protected boolean isDestroyed; // false
+
     public Building(Point position) {
         super(position);
+    }
+
+    @Override
+    public void destroy() {
+        this.isDestroyed = true;
+    }
+
+    public void cleanup() {
+        if (isDestroyed)
+            super.destroy();
+    }
+
+    public boolean isDestroyed() {
+        return isDestroyed;
     }
 }
