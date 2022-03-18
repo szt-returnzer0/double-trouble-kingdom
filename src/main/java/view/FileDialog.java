758c7678f931b9a505d337801ea47b2e3a680e62
Neszlényi Kálmán Balázs <dpu51t@inf.elte.hu>
2022-03-18 01:36:45 +0100

    Implemented FileDialog class

---
+++ src/main/java/view/FileDialog.java
@@ -0,0 +1,30 @@
+package view;
+
+import model.Map;
+import persistence.FileHandler;
+
+import javax.swing.*;
+import javax.swing.filechooser.FileNameExtensionFilter;
+import java.io.File;
+
+public class FileDialog {
+    private final JFileChooser fileChooser;
+
+    public FileDialog() {
+        this.fileChooser = new JFileChooser(new File(System.getProperty("user.dir")));
+        this.fileChooser.setFileFilter(new FileNameExtensionFilter("DTK Save", "dtk"));
+    }
+
+    public void saveDialog(Map mapRef) {
+        if (fileChooser.showSaveDialog(null) == JFileChooser.APPROVE_OPTION)
+            FileHandler.saveToFile(fileChooser.getSelectedFile(), mapRef);
+    }
+
+    public Map loadDialog() {
+        int result = fileChooser.showOpenDialog(null);
+        if (result == JFileChooser.APPROVE_OPTION)
+            return FileHandler.loadMap(fileChooser.getSelectedFile());
+        else
+            return null;
+    }
+}
