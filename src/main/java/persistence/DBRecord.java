1381f298941c1d602658f44fa54e1af5d4fd43e5
Neszlényi Kálmán Balázs <dpu51t@inf.elte.hu>
2022-03-20 12:06:48 +0100

    Added javaDoc to persistence package classes

--- src/main/java/persistence/DBRecord.java
+++ src/main/java/persistence/DBRecord.java
@@ -3,13 +3,39 @@ package persistence;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+/**
+ * Implementation of DBRecord class for Database INSERT and SELECT queries.
+ */
 public final class DBRecord {
+    /**
+     * Name of Player1.
+     */
     private String p1Name;
+    /**
+     * Name of Player2.
+     */
     private String p2Name;
+    /**
+     * Determines the Winner. 1 -> Player1 won | 2 -> Player2 won | 0 -> draw
+     */
     private int winner;
+    /**
+     * The length of the game in seconds.
+     */
     private int time;
+    /**
+     * The id of the Record.
+     */
     private int id;
 
+    /**
+     * Constructs a new record from String and int parameters.
+     *
+     * @param p1Name name of Player1
+     * @param p2Name name of Player2
+     * @param winner the winner's number
+     * @param time   the game's length in seconds
+     */
     public DBRecord(String p1Name, String p2Name, int winner, int time) {
         this.p1Name = p1Name;
         this.p2Name = p2Name;
@@ -17,6 +43,11 @@ public final class DBRecord {
         this.time = time;
     }
 
+    /**
+     * Constructs a new record from a ResultSet.
+     *
+     * @param resultSet the ResultSet to convert
+     */
     public DBRecord(ResultSet resultSet) {
         try {
             this.id = resultSet.getInt("ID");
@@ -29,44 +60,59 @@ public final class DBRecord {
         }
     }
 
+    /**
+     * Returns the name of Player1.
+     *
+     * @return the name of Player1
+     */
     public String getP1Name() {
         return p1Name;
     }
 
+    /**
+     * Returns the name of Player2.
+     *
+     * @return the name of Player2
+     */
     public String getP2Name() {
         return p2Name;
     }
 
+    /**
+     * Returns the winner's number or 0 for draw.
+     *
+     * @return the winner's number or 0 for draw
+     */
     public int getWinner() {
         return winner;
     }
 
+    /**
+     * Returns the id of the record.
+     *
+     * @return the id of the record
+     */
     public int getId() {
         return id;
     }
 
+    /**
+     * Returns the game's length in seconds.
+     *
+     * @return the game's length in seconds
+     */
     public int getTime() {
         return time;
     }
 
+    /**
+     * Returns the inner state of the record as a String.
+     *
+     * @return the inner state of the record as a String
+     */
     @Override
     public String toString() {
         return "'" + p1Name + "','" + p2Name + "'," + winner + "," + time;
     }
 
-    public String p1Name() {
-        return p1Name;
-    }
-
-    public String p2Name() {
-        return p2Name;
-    }
-
-    public int winner() {
-        return winner;
-    }
-
-    public int time() {
-        return time;
-    }
 }
--- src/main/java/persistence/FileHandler.java
+++ src/main/java/persistence/FileHandler.java
@@ -5,20 +5,42 @@ import model.Map;
 
 import java.io.*;
 
+/**
+ * Implementation of FileHandler class, contains static methods for saving and loading Map and Game state to and from files.
+ */
+// Simplify classes with Factory
 public class FileHandler {
 
+    /**
+     * Saves a Map instance to a specified file.
+     *
+     * @param file the File we want to save to
+     * @param map  the Map instance we want to save
+     */
     public static void saveMap(File file, Map map) {
         saveToFile(file, map, ".dtk");
     }
 
+    /**
+     * Saves a Game instance to a specified file.
+     *
+     * @param file the File we want to save to
+     * @param game the Game instance we want to save
+     */
     public static void saveGame(File file, Game game) {
         game.getDatabase().closeConnection();
         saveToFile(file, game, ".dtk_save");
     }
 
+    /**
+     * The logic of saving an Object state to a specified file with a specified file type.
+     *
+     * @param file the File we want to save to
+     * @param obj  the Object we want to save
+     * @param type the extension of the file we save to
+     */
     public static void saveToFile(File file, Object obj, String type) {
         try {
-
             FileOutputStream fileOut = new FileOutputStream(file + type);
             ObjectOutputStream objectOut = new ObjectOutputStream(fileOut);
             objectOut.writeObject(obj);
@@ -28,10 +50,22 @@ public class FileHandler {
         }
     }
 
+    /**
+     * Loads a Map instance from a specified file.
+     *
+     * @param file the file we want to load from
+     * @return a Map instance from the specified file
+     */
     public static Map loadMap(File file) {
         return (Map) loadFromFile(file);
     }
 
+    /**
+     * Loads a Game instance from a specified file.
+     *
+     * @param file the file we want to load from
+     * @return a Game instance from the specified file
+     */
     public static Game loadGame(File file) {
         Game game = (Game) loadFromFile(file);
         if (game != null) {
@@ -40,6 +74,12 @@ public class FileHandler {
         return game;
     }
 
+    /**
+     * The logic of loading an Object state from a specified file.
+     *
+     * @param file the file we want to load from
+     * @return an Object instance from the specified file
+     */
     private static Object loadFromFile(File file) {
         try {
             FileInputStream fileIn = new FileInputStream(file);
