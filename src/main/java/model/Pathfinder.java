c140a7d6416dc90d88d94047f37cb9e22698fd9d
Neszlényi Kálmán Balázs <dpu51t@inf.elte.hu>
2022-04-07 20:05:00 +0200

    Improved fieldToGraph to use a smaller more efficient graph

--- src/main/java/model/Pathfinder.java
+++ src/main/java/model/Pathfinder.java
@@ -5,15 +5,16 @@ import java.util.ArrayList;
 
 public class Pathfinder {
     Map map;
-    int x;
-    int y;
+    int xLength;
+    int yLength;
     Terrain[][] terrain;
     int[][] graph;
 
     public Pathfinder(Map map) {
         this.map = map;
-        this.x = map.getTiles()[0].length;
-        this.y = map.getTiles().length;
+        this.xLength = map.getTiles()[0].length;
+        this.yLength = map.getTiles().length;
+
         this.terrain = this.map.getTiles();
     }
 
@@ -54,8 +55,8 @@ public class Pathfinder {
     public ArrayList<Point> listToPath(int[] list) {
         ArrayList<Point> path = new ArrayList<>();
         for (int i = 0; i < list.length; ++i) {
-            int posx = i % y;
-            int posy = (i - i % y) / y;
+            int posx = i % yLength;
+            int posy = (i - i % yLength) / yLength;
             if (list[i] == 0) {
                 path.add(new Point(posx, posy));
             }
@@ -65,43 +66,16 @@ public class Pathfinder {
     }
 
     public int[][] fieldToGraph(Soldier s) {
-        ArrayList<String> goodTiles = s.getTerrains();
-        int pcs = x * y;
-        int a;
-        graph = new int[pcs][pcs];
-        for (int i = 0; i < y; ++i) {
-            for (int j = 0; j < x; ++j) {
-                graph[j][i] = -1;
+        graph = new int[yLength][xLength];
+        for (int y = 0; y < yLength; y++) {
+            for (int x = 0; x < xLength; x++) {
+                if (s.getTerrains().contains(map.getTiles()[y][x].getType()) && !(map.getTiles()[y][x].getEntities().size() > 0 && "Castle Barracks Barricade Sniper Shotgun".contains(map.getTiles()[y][x].getEntities().get(0).getType())))
+                    graph[y][x] = map.getTiles()[y][x].getSpeedMod();
+                else
+                    graph[y][x] = -1;
             }
         }
-        for (int i = 0; i < y; ++i) {
-            for (int j = 0; j < x; ++j) {
-                //Up
-                if (j != 0) {
-                    int weight = terrain[i][j - 1].getSpeedMod();
-                    if (!terrain[i][j - 1].getEntities().contains(Building.class) && goodTiles.contains(terrain[i][j - 1].getType()))
-                        graph[j * y + i][(j - 1) * y + i] = weight;
-                }
-                //Right
-                if (i != y - 1) {
-                    int weight = terrain[i + 1][j].getSpeedMod();
-                    if (!terrain[i + 1][j].getEntities().contains(Building.class) && goodTiles.contains(terrain[i + 1][j].getType()))
-                        graph[i * y + j][i * y + j + 1] = weight;
-                }
-                //Down
-                if (j != x - 1) {
-                    int weight = terrain[i][j + 1].getSpeedMod();
-                    if (!terrain[i][j + 1].getEntities().contains(Building.class) && goodTiles.contains(terrain[i][j + 1].getType()))
-                        graph[j * y + i][(j + 1) * y + i] = weight;
-                }
-                //Left
-                if (i != 0) {
-                    int weight = terrain[i - 1][j].getSpeedMod();
-                    if (!terrain[i - 1][j].getEntities().contains(Building.class) && goodTiles.contains(terrain[i - 1][j].getType()))
-                        graph[j * y + i][j * y + (i - 1)] = weight;
-                }
-            }
-        }
+
         return graph;
     }
 }
