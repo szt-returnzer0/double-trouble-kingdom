28f3583b54f08b7fc0b019e1706709626b78c069
Berecz Dániel <c4q4kv@inf.elte.hu>
2022-05-08 22:40:44 +0200

    Implemented test for MapEditorModel

---
+++ src/test/java/MapEditorModelTest.java
@@ -0,0 +1,23 @@
+import DTK_model.*;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class MapEditorModelTest {
+    @Test
+    @DisplayName("Place Block Test")
+    void placeBlockTest() {
+        Map map = new Map();
+        Game game = new Game(map);
+        MapEditorModel mapEditorModel = new MapEditorModel();
+        int x = 0;
+        int y = 0;
+        GameFieldModel gameFieldModel = new GameFieldModel(game);
+        boolean inverted = false;
+        mapEditorModel.placeBlock(x, y, gameFieldModel, inverted, Types.SNIPER);
+        assertEquals(1, Game.getMapReference().getTiles()[0][0].getEntities().size(), "Entities count should be 1");
+        mapEditorModel.placeBlock(x, y, gameFieldModel, inverted, Types.MOUNTAIN);
+        assertEquals(Types.MOUNTAIN, Game.getMapReference().getTiles()[0][0].getType(), "Type should be Mountain");
+    }
+}
